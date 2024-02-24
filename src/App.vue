b144c8dcd2fb72eb9a8708855c8b3868444fbb4a
filippogiovannoni <filippogiovannonii@gmail.com>
2024-02-24 10:38:51 +0100

    add AppFooter component

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
 import { store } from '../src/store.js'
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
-  components: { AppHeader, AppMain },
+  components: { AppHeader, AppMain, AppFooter },
   data() {
     return {
       store,
@@ -18,6 +19,7 @@ export default {
 <template>
   <AppHeader></AppHeader>
   <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
 <style></style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+<template>
+    <footer>
+        <div>
+            Made by Booleaner @filippogiovannoni with love ❤️
+        </div>
+    </footer>
+</template>
+
+
+<style scoped>
+footer {
+    padding: 3rem 0;
+    filter: drop-shadow(0 0 5px var(--bool-primary));
+    width: 100%;
+    background-color: black;
+    text-align: center;
+    font-style: italic;
+    font-size: 12px;
+}
+</style>
