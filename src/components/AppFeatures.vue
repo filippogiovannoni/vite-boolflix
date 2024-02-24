ae02f39b13f66c40f09b247811dea24c0b6e8897
filippogiovannoni <filippogiovannonii@gmail.com>
2024-02-24 11:01:21 +0100

    add AppFeature component

---
+++ src/components/AppFeatures.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppFeatures'
+}
+</script>
+<template>
+    <div class="features">
+        <ul>
+            <li><a class="active" href="#">Sfoglia</a></li>
+            <li><a href="#">Kids</a></li>
+        </ul>
+    </div>
+</template>
+
+
+<style scoped>
+ul {
+    display: flex;
+    list-style: none;
+    gap: 1rem;
+
+    & a.active {
+        color: var(--bool-primary);
+    }
+
+    & a {
+        text-decoration: none;
+        color: var(--bool-light);
+    }
+}
+</style>
