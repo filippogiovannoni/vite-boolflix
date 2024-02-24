d657e9252d76a9748edb9cb5d26b769532acfdab
filippogiovannoni <filippogiovannonii@gmail.com>
2024-02-24 11:30:46 +0100

    add AppSearch component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,16 +2,17 @@
 
 import { store } from '../store.js'
 import AppLogo from './AppLogo.vue';
+import AppFeatures from './AppFeatures.vue'
+import AppSearch from './AppSearch.vue'
 
 export default {
     name: 'AppHeader',
     data() {
         return {
-            store,
-            searchBar: ''
+            store
         };
     },
-    components: { AppLogo }
+    components: { AppLogo, AppFeatures, AppSearch }
 }
 </script>
 
@@ -19,18 +20,8 @@ export default {
     <header>
         <nav class="container">
             <AppLogo></AppLogo>
-            <div class="features">
-                <ul>
-                    <li><a class="active" href="#">Sfoglia</a></li>
-                    <li><a href="#">Kids</a></li>
-                </ul>
-            </div>
-            <div class="search">
-                <input type="text" name="searchBox" id="searchBox" placeholder="Digita il nome del film"
-                    v-model="this.searchBar">
-                <button
-                    @click="store.getMovieUrl(store.base_api_movie_url + this.searchBar); store.getMovieUrl(store.base_api_tv_url + this.searchBar)">Cerca</button>
-            </div>
+            <AppFeatures></AppFeatures>
+            <AppSearch></AppSearch>
         </nav>
     </header>
 </template>
@@ -51,29 +42,5 @@ nav {
     justify-content: space-between;
     align-items: center;
     padding: 1rem 0;
-
-    & ul {
-        display: flex;
-        list-style: none;
-        gap: 1rem;
-
-        & a.active {
-            color: var(--bool-primary);
-        }
-
-        & a {
-            text-decoration: none;
-            color: var(--bool-light);
-        }
-    }
-
-    .search>input,
-    button {
-        border-radius: 0.25rem;
-        border-style: none;
-        padding: 0.25rem;
-        margin: 0 0.25rem;
-    }
-
 }
 </style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,33 @@
+<script>
+
+import { store } from '../store.js';
+
+export default {
+    name: 'AppSearch',
+    data() {
+        return {
+            store,
+            searchBar: ''
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="search">
+        <input type="text" name="searchBox" id="searchBox" placeholder="Digita il nome del film" v-model="this.searchBar">
+        <button
+            @click="store.getMovieUrl(store.base_api_movie_url + this.searchBar); store.getMovieUrl(store.base_api_tv_url + this.searchBar)">Cerca</button>
+    </div>
+</template>
+
+
+<style scoped>
+.search>input,
+button {
+    border-radius: 0.25rem;
+    border-style: none;
+    padding: 0.25rem;
+    margin: 0 0.25rem;
+}
+</style>
